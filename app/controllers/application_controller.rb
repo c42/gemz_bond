fda3a8986b9a9cc0fc6ac41d1b76314afe0f846e
Tejas Dinkar <tejas@gja.in>
2012-02-07 23:00:39 +0530

    Adding a logging message when we do not hit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   def get(uri_str, limit = 10)
     raise ArgumentError, 'too many HTTP redirects' if limit == 0
 
+    Rails.logger.warn("Fetching #{uri_str}")
     response = Net::HTTP.get_response(URI(uri_str))
 
     case response
